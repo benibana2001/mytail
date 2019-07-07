349ec28d08ace3060bc5720550988b52d1a3482e
benibana2001 <benibana2001@gmail.com>
2019-07-08 06:11:42 +0900

    fix test

--- tail/tail_test.go
+++ tail/tail_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestTail(t *testing.T) {
-	s := Tail(3, "./forTest.txt")
-	if s[0] != "This is line-18" {
+	mytail := Create(3, "./forTest.txt")
+	if mytail.Data[0] != "This is line-18" {
 		t.Fatal("s[0] is not correct")
 	}
-	if s[1] != "This is line-19" {
+	if mytail.Data[1] != "This is line-19" {
 		t.Fatal("s[1] is not correct")
 	}
-	if s[2] != "This is line-20" {
+	if mytail.Data[2] != "This is line-20" {
 		t.Fatal("s[2] is not correct")
 	}
 }
